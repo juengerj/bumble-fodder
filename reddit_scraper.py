a0cfab9e627fa5aba64d7afa7332ecce42e2a6f6
Jason Juenger <juengerj@flip1.engr.oregonstate.edu>
2020-01-29 16:59:38 -0800

    Add reddit scraper module

---
+++ reddit_scraper.py
@@ -0,0 +1,33 @@
+import praw
+
+# Get attributes of submission object
+#attributes = next(submission).__dict__.keys()
+
+# Get next submission object from listing generator
+#first_post = next(submission)
+
+
+class Reddit:
+
+    def __init__(self, credentials):
+        self.reddit = None
+        self.submission = None
+
+        with open(credentials, 'r') as infile:
+            creds = infile.read().splitlines()
+
+        self.reddit = praw.Reddit(client_id=creds[0], client_secret=creds[1],
+                                    password=creds[3], user_agent='scrapeSpike by bestaccountantevar',
+                                    username=creds[2])
+
+    def get_submissions(self, subreddit):
+        self.submission = self.reddit.subreddit(subreddit).hot(limit=5)
+
+    def get_post_details(self, *args):
+        for i in self.submission:
+            for j in args:
+                print(getattr(i, j))
+
+my_reddit = Reddit('credentials.txt')
+my_reddit.get_submissions('askreddit')
+my_reddit.get_post_details('url', 'thumbnail')
