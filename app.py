c0b341d90ac5b5eb566c727eb7f19ac8ea378397
thielea <52184120+thielea@users.noreply.github.com>
2020-02-08 20:40:18 -0600

    GET requests ok

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, session
+from flask import Flask, request, session, render_template
 #from flask_httpauth import HTTPBasicAuth
 #from flask_sqlalchemy import SQLAlchemy
 #from passlib.apps import custom_app_context as pwd_context
@@ -16,11 +16,14 @@ def get_urls():
         my_reddit.get_submissions(subreddit)
     else:
         my_reddit.get_submissions('dogswithjobs')
-    results = str(my_reddit.get_post_details('url', 'thumbnail'))
-    print(results)
-    return results
+        # my_reddit.get_submissions('funny')
+    results = my_reddit.get_post_details('url', 'thumbnail')
+    # print(results)
+    # return results
+    # results = str(['fish','pony','hip','hop','hip-hop-a-bottom-us'])
 
+    return render_template('index.html', results=results)
 
 if __name__ == '__main__':
     app.debug = True
-    app.run(host='0.0.0.0', port='23234')
+    app.run(host='0.0.0.0', port='4799')
