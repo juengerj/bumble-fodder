4047564d6add69cd5a7f936b170bbc0026556bc1
Joel Johnson <johnsjoe@flip3.engr.oregonstate.edu>
2020-02-11 09:33:14 -0800

    Added delete functionality

--- static/js/mechanics.js
+++ static/js/mechanics.js
@@ -1,6 +1,10 @@
-alert("Testing testing testing");
 
 function deleteSubreddit() {
-  
+    
+}
+
+function removeElement(id) {
+    getObject = document.getElementById(id);
+    getObject.parentNode.removeChild(getObject);
 }
 
