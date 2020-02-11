c7324b81eea9cafbebff0dbe481f2a244a8ff62e
thielea <52184120+thielea@users.noreply.github.com>
2020-02-10 22:45:17 -0600

    Can View Thumbnails!!!

--- app.py
+++ app.py
@@ -6,23 +6,42 @@ import reddit_scraper
 
 app = Flask(__name__, static_folder='static')
 
-my_reddit = reddit_scraper.Reddit('credentials.txt')
+# my_reddit = reddit_scraper.Reddit('credentials.txt')
+my_reddit = reddit_scraper.Reddit.get_instance()
 
 
 @app.route('/', methods=['GET'])
 def get_urls():
     subreddit = request.args.get('subreddit')
+    # print("Getting subreddit", file=sys.stdout)
     if subreddit:
         my_reddit.get_submissions(subreddit)
     else:
         my_reddit.get_submissions('dogswithjobs')
-        # my_reddit.get_submissions('funny')
+    
+    # print("storing subreddit", file=sys.stdout)
+    
     results = my_reddit.get_post_details('url', 'thumbnail')
-    # print(results)
+    urlsGoHere=results[::2]
+    thumbnailsGoHere=results[1::2]
+    # data={'urls':urlsGoHere, 'thumbnails':thumbnailsGoHere,'ids':idsGoHere}
+    data = []
+    for i in range(0,5):
+        data.append({'thumbnail':thumbnailsGoHere[i],'url':urlsGoHere[i],'id':i})
     # return results
     # results = str(['fish','pony','hip','hop','hip-hop-a-bottom-us'])
 
-    return render_template('index.html', results=results)
+    return render_template('index.html', data=data)
+
+# @app.route('/top5', methods=['GET'])
+# NUM_OF_SUBREDDITS = 5
+# def get_top5():
+#     top5subreddits[] = []
+#     for i in range(NUM_OF_SUBREDDITS):
+#         top5subreddits[i] = 
+
+
+
 
 if __name__ == '__main__':
     app.debug = True
--- reddit_scraper.py
+++ reddit_scraper.py
@@ -9,11 +9,25 @@ import praw
 
 class Reddit:
 
-    def __init__(self, credentials):
+    __instance = None
+
+    @staticmethod
+    def get_instance():
+        if Reddit.__instance == None:
+            Reddit()
+        return Reddit.__instance
+
+    def __init__(self):
         self.reddit = None
         self.submission = None
+        self.credentials = 'credentials.txt'
 
-        with open(credentials, 'r') as infile:
+        if Reddit.__instance != None:
+            raise Exception("Error: class is a singleton and object %s exists" % Reddit.__instance)
+        else:
+            Reddit.__instance = self
+
+        with open(self.credentials, 'r') as infile:
             creds = infile.read().splitlines()
 
         self.reddit = praw.Reddit(client_id=creds[0], client_secret=creds[1],
