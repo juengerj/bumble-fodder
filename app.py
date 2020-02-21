36a7507a5d3176d4d9e67cc174471749b1e8ed6a
Michael <michaelpaulmccabe@gmail.com>
2020-02-21 15:06:28 -0800

    added login

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, flash, redirect, url_for
+from flask_login import UserMixin, LoginManager, login_user, logout_user, current_user, login_required
+from forms import LoginForm
+from werkzeug.security import generate_password_hash, check_password_hash
+
 # from flask_httpauth import HTTPBasicAuth
 from flask_sqlalchemy import SQLAlchemy
 
@@ -9,7 +13,33 @@ import consts
 app = Flask(__name__, static_folder='static')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user_db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = "we are great"
+
 db = SQLAlchemy(app)
+login = LoginManager(app)
+login.init_app(app)
+login.login_view = "login"
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'user'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 # my_reddit = reddit_scraper.Reddit('credentials.txt')
 my_reddit = reddit_scraper.Reddit.get_instance()
@@ -64,6 +94,23 @@ def get_urls():
 #     for i in range(NUM_OF_SUBREDDITS):
 #         top5subreddits[i] = 
 
+# RIPPED
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        # if not next_page or url_parse(next_page).netloc != '':
+        #     next_page = url_for('index')
+        return redirect(next_page)
+    return render_template('login.html', title='Sign In', form=form)
 
 if __name__ == '__main__':
     # if not os.path.exists('user_db.sqlite'):
