901234586d48ac61fe2e809f6f08c827c592c953
Joel Johnson <johnsjoe@flip3.engr.oregonstate.edu>
2020-02-29 18:16:17 -0800

    final touches

--- consts.py
+++ consts.py
@@ -1,6 +1,6 @@
 """Variables used globally throughout the app"""
 
 HOST = '0.0.0.0'
-PORT = '23234'
+PORT = '2005'
 NUM_SUBS = 5                # number of subreddits to scrape
 NUM_TOP = 3                 # number of top posts to display
